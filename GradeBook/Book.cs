5891987dd31ff40fcbecbb01ca3525334ff6cb44
asalasan <adi.salasan@gmail.com>
2021-09-28 05:31:44 +0300

    Final, "beautyfied" version with separate files for everything ...etc

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -1,32 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace GradeBook
 {
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
-
-    public class NamedObject
-    {
-        public NamedObject(string name)
-        {
-            Name = name;
-        }
-
-        public string Name
-        {
-            get;
-            set;
-        }
-    }
-
-    public interface IBook
-    {
-        void AddGrade(double grade);
-        Statistics GetStatistics();
-        string Name { get; }
-        event GradeAddedDelegate GradeAdded;
-    }
 
     public abstract class Book : NamedObject, IBook
     {
@@ -37,45 +14,6 @@ namespace GradeBook
         public abstract event GradeAddedDelegate GradeAdded;
         public abstract void AddGrade(double grade);
         public abstract Statistics GetStatistics();
-    }
-
-    public class DiskBook : Book
-    {
-        public DiskBook(string name) : base(name)
-        {
-        }
-
-        public override event GradeAddedDelegate GradeAdded;
-
-        public override void AddGrade(double grade)
-        {
-            using (var writer = File.AppendText($"{Name}.txt"))
-            {
-                writer.WriteLine(grade);
-                if (GradeAdded != null)
-                {
-                    GradeAdded(this, new EventArgs());
-                }
-            }
-        }
-
-        public override Statistics GetStatistics()
-        {
-            var result = new Statistics();
-
-            using (var reader = File.OpenText($"{Name}.txt"))
-            {
-                var line = reader.ReadLine();
-                while (line != null)
-                {
-                    var number = double.Parse(line);
-                    result.Add(number);
-                    line = reader.ReadLine();
-                }
-            }
-
-            return result;
-        }
     }
 
     public class InMemoryBook : Book
