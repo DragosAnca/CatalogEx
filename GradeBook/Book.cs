89011cd4b5202e0af7e9645749824e05b5177396
asalasan <adi.salasan@gmail.com>
2021-09-28 05:38:39 +0300

    Moved InMemory class outside as well

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -14,48 +13,5 @@ namespace GradeBook
         public abstract event GradeAddedDelegate GradeAdded;
         public abstract void AddGrade(double grade);
         public abstract Statistics GetStatistics();
-    }
-
-    public class InMemoryBook : Book
-    {
-        public InMemoryBook(string name) : base(name)
-        {
-            grades = new List<double>();
-            Name = name;
-        }
-
-        public override void AddGrade(double grade)
-        {
-            if (grade <= 100 && grade >= 0)
-            {
-                grades.Add(grade);
-                if (GradeAdded != null)
-                {
-                    GradeAdded(this, new EventArgs());
-                }
-            }
-            else
-            {
-                throw new ArgumentException($"Invalid {nameof(grade)}");
-            }
-        }
-
-        public override event GradeAddedDelegate GradeAdded;
-
-        public override Statistics GetStatistics()
-        {
-            var result = new Statistics();
-
-            for (var index = 0; index < grades.Count; index += 1)
-            {
-                result.Add(grades[index]);
-            }
-
-            return result;
-        }
-
-        private List<double> grades;
-
-        public const string CATEGORY = "Science";
     }
 }
